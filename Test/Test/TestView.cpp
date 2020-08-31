62ca49b8fb208e36da02fea3a32365be02307d4e
2356131235 <2356131235@qq.com>
2020-08-31 17:00:14 +0800

    创建解决方案

---
+++ Test/Test/TestView.cpp
@@ -0,0 +1,113 @@
+﻿
+// TestView.cpp: CTestView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "Test.h"
+#endif
+
+#include "TestDoc.h"
+#include "TestView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CTestView
+
+IMPLEMENT_DYNCREATE(CTestView, CFormView)
+
+BEGIN_MESSAGE_MAP(CTestView, CFormView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CFormView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CFormView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CFormView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CTestView 构造/析构
+
+CTestView::CTestView() noexcept
+	: CFormView(IDD_TEST_FORM)
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CTestView::~CTestView()
+{
+}
+
+void CTestView::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+}
+
+BOOL CTestView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CFormView::PreCreateWindow(cs);
+}
+
+void CTestView::OnInitialUpdate()
+{
+	CFormView::OnInitialUpdate();
+	GetParentFrame()->RecalcLayout();
+	ResizeParentToFit();
+	a = 10;
+
+}
+
+
+// CTestView 打印
+
+BOOL CTestView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CTestView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CTestView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CTestView::OnPrint(CDC* pDC, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 在此处添加自定义打印代码
+}
+
+
+// CTestView 诊断
+
+#ifdef _DEBUG
+void CTestView::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+void CTestView::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+
+CTestDoc* CTestView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CTestDoc)));
+	return (CTestDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CTestView 消息处理程序
---
+++ Test/Test/TestView.h
@@ -0,0 +1,58 @@
+﻿
+// TestView.h: CTestView 类的接口
+//
+
+#pragma once
+
+
+class CTestView : public CFormView
+{
+protected: // 仅从序列化创建
+	CTestView() noexcept;
+	DECLARE_DYNCREATE(CTestView)
+
+public:
+#ifdef AFX_DESIGN_TIME
+	enum{ IDD = IDD_TEST_FORM };
+#endif
+
+// 特性
+public:
+	CTestDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+	virtual void OnInitialUpdate(); // 构造后第一次调用
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnPrint(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CTestView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	int a;
+};
+
+#ifndef _DEBUG  // TestView.cpp 中的调试版本
+inline CTestDoc* CTestView::GetDocument() const
+   { return reinterpret_cast<CTestDoc*>(m_pDocument); }
+#endif
+
